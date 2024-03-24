58c047a5c3c00f9a9fe02132e72029c7d1b01048
woozie-10 <deriuhin.mykhailo@gmail.com>
2024-03-24 07:10:52 +0200

    added swagger

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	_ "github.com/woozie-10/api-clean-arch/docs"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/woozie-10/api-clean-arch/internal/database"
@@ -12,7 +13,7 @@ import (
 	"github.com/woozie-10/api-clean-arch/student"
 )
 
-const defaultAddress = ":9090"
+const defaultAddress = "9090"
 
 func init() {
 	err := godotenv.Load()
@@ -20,6 +21,12 @@ func init() {
 		log.Fatal("Error loading .env file")
 	}
 }
+
+// @title Students REST API
+// @description This is a  API for managing students.
+// @version 1.0
+// @host localhost:9090
+// @BasePath /
 func main() {
 	dbName := os.Getenv("DATABASE_NAME")
 	collectionName := os.Getenv("COLLECTION_NAME")
@@ -36,7 +43,7 @@ func main() {
 	if address == "" {
 		address = defaultAddress
 	}
-	if err := g.Run("0.0.0.0:"+address); err!=nil{
+	if err := g.Run("0.0.0.0:" + address); err != nil {
 		log.Fatal("Server startup error")
 	}
 }
--- internal/rest/student.go
+++ internal/rest/student.go
@@ -6,12 +6,16 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/woozie-10/api-clean-arch/domain"
+	swaggerFiles "github.com/swaggo/files"
+    "github.com/swaggo/gin-swagger"
 )
 
 type ResponseError struct {
 	Message string `json:"message"`
 }
-
+type Response struct {
+	Message string `json:"message"`
+}
 type StudentService interface {
 	Get(ctx context.Context) ([]*domain.Student, error)
 	GetByUsername(ctx context.Context, username string) (*domain.Student, error)
@@ -30,14 +34,26 @@ func NewStudentHandler(g *gin.Engine, svc StudentService) {
 	handler := &StudentHandler{
 		Service: svc,
 	}
-	g.GET("", handler.Get)
-	g.GET("/:username", handler.GetByUsername)
-	g.GET("/group/:group", handler.GetByGroup)
-	g.GET("course/:course", handler.GetByCourse)
-	g.POST("", handler.Add)
-	g.PUT("/:username", handler.Update)
-	g.DELETE("/:username", handler.Delete)
+	g.GET("/students", handler.Get)
+	g.GET("/students/:username", handler.GetByUsername)
+	g.GET("/students/group/:group", handler.GetByGroup)
+	g.GET("/students/course/:course", handler.GetByCourse)
+	g.POST("/students", handler.Add)
+	g.PUT("/students/:username", handler.Update)
+	g.DELETE("/students/:username", handler.Delete)
+
+	g.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 }
+// Get godoc
+// @Summary Retrieve a list of students
+// @Description Returns a list of all students from the database
+// @Tags students
+// @Accept json
+// @Produce json
+// @Success 200 {array} domain.Student "List of students"
+// @Failure 500 {object} ResponseError "Internal server error"
+// @Router /students [get]
 func (s *StudentHandler) Get(c *gin.Context) {
 	ctx := c.Request.Context()
 	students, err := s.Service.Get(ctx)
@@ -47,18 +63,38 @@ func (s *StudentHandler) Get(c *gin.Context) {
 	}
 	c.IndentedJSON(http.StatusOK, students)
 }
-
+// GetByUsername godoc
+// @Summary Get student by username
+// @Description Retrieve a student by their username from the database
+// @Tags students
+// @Accept json
+// @Produce json
+// @Param username path string true "Username of the student to retrieve"
+// @Success 200 {object} domain.Student "Student details"
+// @Failure 400 {object} ResponseError "Bad request"
+// @Failure 500 {object} ResponseError "Internal server error"
+// @Router /students/{username} [get]
 func (s *StudentHandler) GetByUsername(c *gin.Context) {
-	username := c.Param("username")
-	ctx := c.Request.Context()
-	student, err := s.Service.GetByUsername(ctx, username)
-	if err != nil {
-		c.IndentedJSON(http.StatusInternalServerError, ResponseError{Message: err.Error()})
-		return
-	}
-	c.IndentedJSON(http.StatusOK, student)
+    username := c.Param("username")
+    ctx := c.Request.Context()
+    student, err := s.Service.GetByUsername(ctx, username)
+    if err != nil {
+        c.IndentedJSON(http.StatusInternalServerError, ResponseError{Message: err.Error()})
+        return
+    }
+    c.IndentedJSON(http.StatusOK, student)
 }
-
+// GetByGroup godoc
+// @Summary Get students by group
+// @Description Retrieve a list of students by their group from the database
+// @Tags students
+// @Accept json
+// @Produce json
+// @Param group path string true "Group of the students to retrieve"
+// @Success 200 {array} domain.Student "List of students"
+// @Failure 400 {object} ResponseError "Bad request"
+// @Failure 500 {object} ResponseError "Internal server error"
+// @Router /students/group/{group} [get]
 func (s *StudentHandler) GetByGroup(c *gin.Context) {
 	group := c.Param("group")
 	ctx := c.Request.Context()
@@ -69,7 +105,17 @@ func (s *StudentHandler) GetByGroup(c *gin.Context) {
 	}
 	c.IndentedJSON(http.StatusOK, students)
 }
-
+// GetByCourse godoc
+// @Summary Get students by course
+// @Description Retrieve a list of students by their course from the database
+// @Tags students
+// @Accept json
+// @Produce json
+// @Param course path string true "Course of the students to retrieve"
+// @Success 200 {array} domain.Student "List of students"
+// @Failure 400 {object} ResponseError "Bad request"
+// @Failure 500 {object} ResponseError "Internal server error"
+// @Router /students/course/{course} [get]
 func (s *StudentHandler) GetByCourse(c *gin.Context) {
 	course := c.Param("course")
 	ctx := c.Request.Context()
@@ -80,12 +126,22 @@ func (s *StudentHandler) GetByCourse(c *gin.Context) {
 	}
 	c.IndentedJSON(http.StatusOK, students)
 }
-
+// Add godoc
+// @Summary Add a new student
+// @Description Add a new student to the database
+// @Tags students
+// @Accept json
+// @Produce json
+// @Param student body domain.Student true "Student object to add"
+// @Success 201 {object} domain.Student "Added student"
+// @Failure 422 {object} ResponseError "Unprocessable entity"
+// @Failure 500 {object} ResponseError "Internal server error"
+// @Router /students [post]
 func (s *StudentHandler) Add(c *gin.Context) {
 	var student *domain.Student
 
     if err := c.ShouldBindJSON(&student); err != nil {
-        c.JSON(http.StatusUnprocessableEntity, ResponseError{err.Error()}})
+        c.JSON(http.StatusUnprocessableEntity, ResponseError{err.Error()})
         return
     }
 
@@ -97,23 +153,41 @@ func (s *StudentHandler) Add(c *gin.Context) {
 
     c.JSON(http.StatusCreated, student)
 }
-
+// Update godoc
+// @Summary Update a student
+// @Description Update an existing student in the database
+// @Tags students
+// @Accept json
+// @Produce json
+// @Param username path string true "Username of the student to update"
+// @Param newStudent body domain.Student true "New student data"
+// @Success 200 {object} domain.Student "Updated student"
+// @Failure 422 {object} ResponseError "Unprocessable entity"
+// @Failure 500 {object} ResponseError "Internal server error"
+// @Router /students/{username} [put]
 func (s *StudentHandler) Update(c *gin.Context) {
 	var newStudent *domain.Student
 	username := c.Param("username")
 	if err := c.ShouldBindJSON(&newStudent); err != nil {
-        c.JSON(http.StatusUnprocessableEntity, ResponseError{err.Error()}})
+        c.JSON(http.StatusUnprocessableEntity, ResponseError{err.Error()})
         return
     }
 	ctx := c.Request.Context()
-	err = s.Service.Update(ctx, username, newStudent)
+	err := s.Service.Update(ctx, username, newStudent)
 	if err != nil {
 		c.IndentedJSON(http.StatusInternalServerError, ResponseError{Message: err.Error()})
 		return
 	}
 	c.IndentedJSON(http.StatusOK, newStudent)
 }
-
+// Delete godoc
+// @Summary Delete a student
+// @Description Delete an existing student from the database
+// @Tags students
+// @Param username path string true "Username of the student to delete"
+// @Success 200 {object} Response "Message indicating successful deletion"
+// @Failure 500 {object} ResponseError "Internal server error"
+// @Router /students/{username} [delete]
 func (s *StudentHandler) Delete(c *gin.Context) {
 	username := c.Param("username")
 	ctx := c.Request.Context()
@@ -122,5 +196,5 @@ func (s *StudentHandler) Delete(c *gin.Context) {
 		c.IndentedJSON(http.StatusInternalServerError, ResponseError{Message: err.Error()})
 		return
 	}
-	c.IndentedJSON(http.StatusOK, gin.H{"message": "the student was successfully removed"})
+	c.IndentedJSON(http.StatusOK, Response{Message: "the student was successfully removed"})
 }
